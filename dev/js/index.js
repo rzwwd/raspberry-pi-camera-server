67b57fae8d19cab430c21bd133d26c093136fee7
Serhiy Mytrovtsiy <mitrovtsiy@ukr.net>
2017-04-20 00:03:41 +0200

    Update index.js

--- dev/js/index.js
+++ dev/js/index.js
@@ -2,6 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import createReactClass from 'create-react-class';
 const driveCar = false;
+let socket = NaN;
+
+let moveStatus = 0;
+let now_left = 0;
+let now_right = 0;
+let now_ahead = 0;
+let now_back = 0;
+let interval_to_move_1_1 = false;
+let interval_to_move_1_2 = false;
+let interval_to_move_2_1 = false;
+let interval_to_move_2_2 = false;
+let state = {
+    rotate: 0,
+    drive: 0
+};
+let sendInterval = NaN;
 
 
 const Home = createReactClass({
@@ -12,9 +28,23 @@ const Home = createReactClass({
             fps: 0
         };
     },
+    componentWillMount: function componentWillMount() {
+        if(driveCar === true){
+            let _this = this;
+            document.addEventListener("keydown", _this.moveByKey, false);
+            document.addEventListener("keyup", _this.stopByKey, false);
+        }
+    },
+    componentWillUnmount: function componentWillUnmount() {
+        if(driveCar === true){
+            let _this = this;
+            document.removeEventListener("keydown", _this.moveByKey, false);
+            document.removeEventListener("keyup", _this.stopByKey, false);
+        }
+    },
     componentDidMount(){
         let _this = this;
-        const socket = io('http://'+self.location.hostname+':2281');
+        socket = io('http://'+self.location.hostname+':2281');
         const joystick = require('nipplejs').create({
             zone: this.refs.joystick,
             mode: 'static',
@@ -27,40 +57,33 @@ const Home = createReactClass({
         this.refs.joystick.style.display = "none";
         this.refs.fps.style.display = "none";
 
-        joystick.on('move', function(evt, data) {
-            /*
-                MAX DISTANCE = 70
-                up = 0
-                down = 1
-                left = 2
-                right = 3
-             */
-            let state = {
-                direction: null,
-                value: null
-            };
+        joystick.on("start", function () {
+            sendInterval = setInterval(function () {
+                socket.emit("move", state);
+            }, 1);
+        }).on('move', function(evt, data) {
             if(data.direction){
                 if (data.direction.angle==="up"){
-                    state.direction = 0;
+                    state.drive = data.distance;
                 }
                 else if (data.direction.angle==="down"){
-                    state.direction = 1;
+                    state.drive = -data.distance;
                 }
                 else if (data.direction.angle==="left"){
-                    state.direction = 2;
+                    state.rotate = data.distance;
                 }
                 else if (data.direction.angle==="right"){
-                    state.direction = 3;
+                    state.rotate = -data.distance;
                 }
-                state.value = data.distance;
-                socket.emit("move", state);
             }
-
-            /*
-            let xhr = new XMLHttpRequest();
-            xhr.open('POST', "http://localhost:2283/move", true);
-            xhr.send(JSON.stringify(state));
-            */
+        }).on("end", function () {
+            if(interval_to_move_1_1 === false &&
+                interval_to_move_1_2 === false &&
+                interval_to_move_2_1 === false &&
+                interval_to_move_2_2 === false){
+                clearInterval(sendInterval);
+                socket.emit("stop_drive", state);
+            }
         });
 
         let startTime = new Date().getTime();
@@ -68,7 +91,6 @@ const Home = createReactClass({
         socket.on('liveStream', function(data) {
             if(new Date().getTime() - startTime >= 1000){
                 startTime = new Date().getTime();
-                //console.log(i + " fps");
                 _this.setState({fps: i});
                 i = 0;
             }
@@ -110,6 +132,92 @@ const Home = createReactClass({
             this.state.socket.emit('stopStream', 0);
             this.refs.stream.src = 'data:image/jpeg;base64,';
             this.setState({status: false});
+        }
+    },
+    moveByKey(e){
+        if (e.keyCode === 38) this.move(1);
+        else if (e.keyCode === 37) this.move(2);
+        else if (e.keyCode === 39) this.move(3);
+        else if (e.keyCode === 40) this.move(4);
+    },
+    stopByKey(e){
+        if (e.keyCode === 38) this.stopMove_1();
+        else if (e.keyCode === 37) this.stopMove_2();
+        else if (e.keyCode === 39) this.stopMove_2();
+        else if (e.keyCode === 40) this.stopMove_1();
+        if(interval_to_move_1_1 === false &&
+            interval_to_move_1_2 === false &&
+            interval_to_move_2_1 === false &&
+            interval_to_move_2_2 === false){
+            socket.emit("stop_drive", state);
+        }
+    },
+    move: function move(e) {
+        moveStatus = e;
+        sendInterval = setInterval(function () {
+            socket.emit("move", state);
+        }, 1);
+        if (e === 1 || e === 4) {
+            if (e === 1 && interval_to_move_1_1 === false) {
+                interval_to_move_1_1 = setInterval(function () {
+                    now_ahead += 5;
+                    if (now_ahead <= 70) state.drive = now_ahead;
+                    else clearInterval(interval_to_move_1_1);
+                }, 50);
+            }
+            else if (e === 4 && interval_to_move_1_2 === false) {
+                interval_to_move_1_2 = setInterval(function () {
+                    now_back -= 5;
+                    if (now_back >= -70) state.drive = now_back;
+                    else clearInterval(interval_to_move_1_2);
+                }, 50);
+            }
+        } else if (e === 2 || e === 3) {
+            if (e === 2 && interval_to_move_2_1 === false) {
+                interval_to_move_2_1 = setInterval(function () {
+                    now_left += 5;
+                    if (now_left <= 70) state.rotate = now_left;
+                    else clearInterval(interval_to_move_2_1);
+                }, 25);
+            } else if (e === 3 && interval_to_move_2_2 === false) {
+                interval_to_move_2_2 = setInterval(function () {
+                    now_right -= 5;
+                    if (now_right >= -70) state.rotate = now_right;
+                    else clearInterval(interval_to_move_2_2);
+                }, 25);
+            }
+        }
+    },
+    stopMove_1: function stopMove_1() {
+        if (now_ahead !== 0 || interval_to_move_1_1 !== false) {
+            clearInterval(interval_to_move_1_1);
+            interval_to_move_1_1 = false;
+            now_ahead = 0;
+            state.drive = 0;
+            if (now_back===0) clearInterval(sendInterval);
+        }
+        if (now_back !== 0 || interval_to_move_1_2 !== false) {
+            clearInterval(interval_to_move_1_2);
+            interval_to_move_1_2 = false;
+            now_back = 0;
+            state.drive = 0;
+            if (now_ahead===0) clearInterval(sendInterval);
+        }
+    },
+    stopMove_2: function stopMove_2() {
+        if (now_left !== 0 || interval_to_move_2_1 !== false) {
+            clearInterval(interval_to_move_2_1);
+            interval_to_move_2_1 = false;
+            now_left = 0;
+            state.rotate = 0;
+            if (now_right===0) clearInterval(sendInterval);
+        }
+        if (now_right !== 0 || interval_to_move_2_2 !== false) {
+            clearInterval(interval_to_move_2_2);
+            interval_to_move_2_2 = false;
+            now_right = 0;
+            state.rotate = 0;
+            if (now_left===0) clearInterval(sendInterval);
         }
     }
 });
